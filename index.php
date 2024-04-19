c97cd2d29063827db49702bbf6a38797dca526f8
harsh fatepara <harshfatepara8484@gmail.com>
2024-04-19 17:41:58 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+// $name = "php programming";
+// print($name);
+// echo gettype($name);
+
+// array : collection of data 
+// $data = ["apple","graphes","pineapple"];
+// echo gettype($data);
+
+
+// typecasting
+// $age = 4.5;
+// $harsh = (int)($age);
+// echo $harsh;
+
+// print elements
+// $data = ["apple","graphes","pineapple"];
+// print($data[0]).PHP_EOL;
+// print($data[1]).PHP_EOL;
+// print($data[2]).PHP_EOL;
+
+// push and pop
+// $data = ["apple","graphes","pineapple"];
+// array_push($data,"harsh");
+// print_r($data);
+// array_pop($data);
+// print_r($data);
+
+// operators
+// arithmatic : + - * / %
+// relational/Comparison : < > <= >= == !=
+
+// looping
+//  $data = ["apple","graphes","pineapple"];
+// $i=0;
+// while($i<3){
+//     print($data[$i]).PHP_EOL;
+//     $i++;
+// }
+
+// for ($i=0; $i <3 ; $i++) { 
+//     print($data[$i]).PHP_EOL;
+// }
+
+// foreach ($data as $key) {
+//     print($key).PHP_EOL;
+      
+// }
+
+// { impload operation on string
+    // give a string where each array elements is seperate by given seperate value
+//  $array = ['lastname', 'email', 'phone'];
+//  $s=implode(",", $array)); // string(20) "lastname,email,phone"
+//  print_r($s);
+
+//  // Empty string when using an empty array:
+//  var_dump(implode('hello', [])); // string(0) ""
+
+//  // The separator is optional:
+//  var_dump(implode(['a', 'b', 'c'])); // string(3) "abc" 
+//  }
+
+// {    join operation on string 
+//     same as impload operation only name is changed (alias of impload)
+//   $array = ['lastname', 'email', 'phone'];
+//     $s = join(",",$arry);
+//     print_r($s);
+// }
+
+// {  lcfirst function on string 
+//     give a string where  first elements of string into lower case
+//     $name="HELLO WORD";
+//     echo lcfirst($name);
+// }
+
+// {  levenshtein operation on string 
+//     give a value that will tell how many change is used to convert one string to given string (give levenshtein distance (The Levenshtein distance is a number that tells you how different two strings are.))
+    // $s="harsh";
+    // $h="hassh";
+    // $d=levenshtein($s,$h);
+    // echo $d;
+
+// }
+
+
+?>
